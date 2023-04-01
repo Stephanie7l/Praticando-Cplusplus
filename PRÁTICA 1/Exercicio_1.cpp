e22baf7ae3d62811d2b33ab204897ead5ada1638
Stéphanie Barbosa <100887395+Stephanie7l@users.noreply.github.com>
2023-04-01 14:09:32 -0300

    Create Exercicio_1.cpp

---
+++ PRÁTICA 1/Exercicio_1.cpp
@@ -0,0 +1,17 @@
+/*==============================================================================
+PRÁTICA 1 - EXERCÍCIO 1: Imprima "Hello World" na tela
+Made by Stéphanie Pereira Barbosa on 01/04/2023
+==============================================================================*/
+
+#include <iostream>  
+#include <string>   
+
+using namespace std;   
+
+int main()
+{
+    string texto = "Hello World";   
+    cout << texto << endl;  
+    
+    return 0;
+}
